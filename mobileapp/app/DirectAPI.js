8d72c33b50a686d07f9d9c4bd0dd5e07ec92031f
Jason Johnston <jason+github@lojjic.com>
2013-11-07 14:50:00 -0700

    Update mobileapp to Architect 3.0 and Touch 2.3

---
+++ mobileapp/app/DirectAPI.js
@@ -0,0 +1,9 @@
+Ext.define('Fulfill.DirectAPI', {
+    requires: ['Ext.direct.*']
+}, function() {
+    var Loader = Ext.Loader,
+        wasLoading = Loader.isLoading;
+    Loader.loadScriptFile('/directapi', Ext.emptyFn, Ext.emptyFn, null, true);
+    Loader.isLoading = wasLoading;
+    Ext.direct.Manager.addProvider(Fulfill.ss.REMOTING_API);
+});
